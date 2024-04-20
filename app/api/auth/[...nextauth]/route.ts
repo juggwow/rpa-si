4947bd6f27a57e5e6d22fdce57ff3fee62c14a61
patna <p.wiriyar@gmail.com>
2024-04-20 13:32:10 +0700

    move authOption

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,7 @@
+import { authOptions } from "@/auth";
 import NextAuth, { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET as string,
-  session: {
-    strategy: 'jwt',
-    maxAge: 15 * 60, // secound
-   },
-};
 
 const handler = NextAuth(authOptions);
 
